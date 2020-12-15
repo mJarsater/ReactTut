3cf83002cf8756c5f0b7c3d259dacd44547ed024
mJarsater <martin.jarsater@hotmail.com>
2020-12-15 09:22:22 +0100

    Init commit

---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Ninjas extends Component {
+    render() {
+        return (
+            <div className="ninja">
+                <div>Name: Martin</div>
+                <div>Age: 25</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
